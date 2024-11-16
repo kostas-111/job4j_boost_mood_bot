5602e09c56edd57d3b27ef715862636930ee3810
kostas-111 <galkink370@gmail.com>
2024-11-16 21:14:04 +0300

    @Profile | Пользователи системы [#505303] Реализована модель User и интерфейс UserFakeRepository

--- src/main/java/ru/job4j/bmb/repository/UserFakeRepository.java
+++ src/main/java/ru/job4j/bmb/repository/UserFakeRepository.java
@@ -3,7 +3,6 @@ package ru.job4j.bmb.repository;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 import ru.job4j.bmb.model.User;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
--- src/main/java/ru/job4j/bmb/repository/UserRepository.java
+++ src/main/java/ru/job4j/bmb/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package ru.job4j.bmb.repository;
 
+import ru.job4j.bmb.model.User;
+import java.util.List;
+
 /*
 Компонент хранения данных
 Класс, который отвечает за хранение данных о пользователях
  */
-
-import ru.job4j.bmb.model.User;
-import java.util.List;
 
 public interface UserRepository {
     List<User> findAll();
