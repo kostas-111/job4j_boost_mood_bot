a943aa67e7f8c749e42b9ac55c7dbb3086530f6c
kostas-111 <galkink370@gmail.com>
2024-12-12 23:03:37 +0300

    @Component и @Bean | Рефакторинг класса TgServiceRemote [#505305] Произвел рефакторинг класса TgServiceRemote, выделив из него компоненты в класс TgUI

--- src/main/java/ru/job4j/bmb/services/TgRemoteService.java
+++ src/main/java/ru/job4j/bmb/services/TgRemoteService.java
@@ -5,16 +5,10 @@ import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.job4j.bmb.model.User;
+import ru.job4j.bmb.repository.MoodRepository;
 import ru.job4j.bmb.repository.UserRepository;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /*
 Telegram предоставлять Rest API для интеграции с сервисом.
@@ -26,18 +20,19 @@ Telegram предоставлять Rest API для интеграции с се
 
 @Service
 public class TgRemoteService extends TelegramLongPollingBot {
-
-    private static final Map<String, String> MOOD_RESP = new HashMap<>();
     private final String botName;
     private final String botToken;
     private final UserRepository userRepository;
+    private final MoodRepository moodRepository;
 
     public TgRemoteService(@Value("${telegram.bot.name}") String botName,
                            @Value("${telegram.bot.token}") String botToken,
-                           UserRepository userRepository) {
+                           UserRepository userRepository,
+                           MoodRepository moodRepository) {
         this.botName = botName;
         this.botToken = botToken;
         this.userRepository = userRepository;
+        this.moodRepository = moodRepository;
     }
 
     /*
@@ -68,9 +63,9 @@ public class TgRemoteService extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasCallbackQuery()) {
-            var data = update.getCallbackQuery().getData();
+            var data = update.getCallbackQuery().getId();
             var chatId = update.getCallbackQuery().getMessage().getChatId();
-            send(new SendMessage(String.valueOf(chatId), MOOD_RESP.get(data)));
+            send(new SendMessage(String.valueOf(chatId), moodRepository.findById(Long.valueOf(data)).toString()));
         }
         if (update.hasMessage() && update.getMessage().hasText()) {
             var message = update.getMessage();
@@ -82,7 +77,6 @@ public class TgRemoteService extends TelegramLongPollingBot {
                 userRepository.save(user);
                 send(sendButtons(chatId));
             }
-
         }
     }
 
@@ -98,43 +92,14 @@ public class TgRemoteService extends TelegramLongPollingBot {
     }
 
     /*
-    Метод для создания кнопки.
-     */
-    InlineKeyboardButton createBtn(String name, String data) {
-        var inline = new InlineKeyboardButton();
-        inline.setText(name);
-        inline.setCallbackData(data);
-        return inline;
-    }
-
-    /*
-    Метод для формирования сообщения, содержащего кнопки.
+    Метод для отправки сообщения, содержащего кнопки.
      */
     public SendMessage sendButtons(long chatId) {
         SendMessage message = new SendMessage();
+        TgUI button = new TgUI(moodRepository);
         message.setChatId(chatId);
         message.setText("Как настроение сегодня?");
-
-        var inlineKeyboardMarkup = new InlineKeyboardMarkup();
-        List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
-
-        keyboard.add(List.of(createBtn("Потерял носок \uD83D\uDE22", "lost_sock")));
-        keyboard.add(List.of(createBtn("Как огурец на полке \uD83D\uDE10", "cucumber")));
-        keyboard.add(List.of(createBtn("Готов к танцам \uD83D\uDE04", "dance_ready")));
-        keyboard.add(List.of(createBtn("Где мой кофе?! \uD83D\uDE23", "need_coffee")));
-        keyboard.add(List.of(createBtn("Слипаются глаза \uD83D\uDE29", "sleepy")));
-
-        inlineKeyboardMarkup.setKeyboard(keyboard);
-        message.setReplyMarkup(inlineKeyboardMarkup);
-
+        message.setReplyMarkup(button.buildButtons());
         return message;
-    }
-
-    static {
-        MOOD_RESP.put("lost_sock", "Носки — это коварные создания. Но не волнуйся, второй обязательно найдётся!");
-        MOOD_RESP.put("cucumber", "Огурец тоже дело серьёзное! Главное, не мариноваться слишком долго.");
-        MOOD_RESP.put("dance_ready", "Супер! Танцуй, как будто никто не смотрит. Или, наоборот, как будто все смотрят!");
-        MOOD_RESP.put("need_coffee", "Кофе уже в пути! Осталось только подождать... И ещё немного подождать...");
-        MOOD_RESP.put("sleepy", "Пора на боковую! Даже супергерои отдыхают, ты не исключение.");
     }
 }
